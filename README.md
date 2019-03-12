c54c6bb39c1721d39e2a67c1ae8b70ea3aced64d
mac <mac@alex-pro.local>
2019-03-12 11:26:51 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,134 @@
 # vue-auto-routes-webpack-plugin
 根据指定目录自动生成 vue-router 配置
+
+
+<h2 align="center">安装</h2>
+
+```bash
+  npm i --save-dev vue-auto-routes-webpack-plugin
+```
+
+```bash
+  yarn add --dev vue-auto-routes-webpack-plugin
+```
+
+<h2 align="center">使用方法</h2>
+
+插件会从指定入口遍历读取[.vue]文件并在指定目录输出一个 vue-router 配置文件。部分路由配置也可在组件内通过 [$$route] 属性声明。
+
+**webpack.config.js**
+```js
+const path = require('path');
+const VueAutoRouteWebpackPlugin = require('vue-auto-routes-webpack-plugin')
+
+function resolve(_path) {
+  return path.resolve(__dirname, _path);
+}
+
+module.exports = {
+  entry: 'index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new VueAutoRouteWebpackPlugin({
+      entry: resolve('src/views/'),
+      output: resolve('src/route/routes.js'),
+      rootComponent: 'Login',
+      indexComponent: 'Index',
+      useFileName: true
+    })
+  ]
+}
+```
+插件会在 [output] 目录下生成 [routes.js] 文件，大概像这样
+
+**src/route/routes.js**
+```js
+
+export default [{
+  "name": "Login",
+  "path": "/",
+  "components": {
+    default: require('/Users/mac/Documents/project-formal/auto-vue-router/src/views/Login.vue').default
+  },
+  "meta": null
+}, {
+  "path": "/main",
+  "children": [{
+    "name": "List",
+    "path": "List",
+    "components": {
+      default: require('/Users/mac/Documents/project-formal/auto-vue-router/src/views/main/List.vue').default
+    },
+    "meta": null
+  }, {
+    "path": "sub",
+    "children": [],
+    "components": {
+      default: require('/Users/mac/Documents/project-formal/auto-vue-router/src/views/main/sub/Index.vue').default
+    },
+    "name": "sub/Index",
+    "meta": null
+  }],
+  "components": {
+    default: require('/Users/mac/Documents/project-formal/auto-vue-router/src/views/main/Index.vue').default
+  },
+  "name": "/main/Index",
+  "meta": null
+}]
+
+```
+
+然后你就可以直接使用这份配置了
+
+**src/route/index.js**
+```js
+
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import routes from './routes'
+
+Vue.use(Router);
+
+routes.push({
+  path: '*',
+  redirect: '/'
+})
+
+export default new Router({
+  routes: routes
+});
+
+```
+
+你还可以在 [.vue] 组件内定义路由的相关信息，但这也不是必须的，只有你需要时才这么做
+
+```js
+export default {
+  name:'login',
+  $$route:{
+    name:'Login',
+    lazy:true,
+    meta:{
+      label:'登录'
+    }
+  }
+
+}
+
+```
+
+<h2 align="center">插件配置参数</h2>
+|Name|Type|Default|Description|require|
+|:--:|:--:|:-----:|:----------|:--|
+|**`entry`**|`{String}`|`无`|路由页面的入口路径|yes|
+|**`output`**|`{String}`|`无`|配置文件输出路径|yes|
+|**`rootComponent`**|`{String}`|`Login.vue`|根路由下的组件，也就是当路由为`/`时的页面，要带有`.vue`后缀哦|no|
+|**`indexComponent`**|`{String}`|`Index`|多级路由时，需要为每级路由提供一个入口，用于放置`<router-view />`承载子路由，【不要】带有`.vue`后缀|no|
+|**`useFileName`**|`{Boolea}`|`false`|是否使用`文件名`作为`路由名称`|no|
+|**`ignoreDir`**|`{String}`|`components`|在插件遍历目录时，需要忽略的目录|no|
+|**`propsKeyName`**|`{String}`|`$$route`|组件内的路由配置key name|no|
+
---
+++ index.js
@@ -0,0 +1,302 @@
+const path = require('path');
+const fs = require('fs');
+const chokidar = require('chokidar');
+const {
+  parse
+} = require('@vue/component-compiler-utils');
+const vueCompiler = require('vue-template-compiler');
+const diff = require('deep-diff').diff;
+
+
+class VueAutoRouteWebapckPlugin {
+  constructor(options) {
+    this.options = Object.assign({
+      rootComponent: 'Login.vue',
+      indexComponent: 'Index',
+      ignoreDir: 'components',
+      useFileName: false,
+      propsKeyName: '$$route'
+    }, options);
+    this.options.entry = path.join(options.entry, '/');
+    this.files = {};
+    this.filesPath = [];
+    this.metaOutput = {
+      warnings: [],
+      errors: []
+    };
+    this.routes = [];
+    this.routesMap = {};
+  }
+
+  apply(compiler) {
+    this._hook(compiler, 'emit', 'emit', (compilation, cb) => {
+      compilation.errors = compilation.errors.concat(this.metaOutput.errors.map(x => '[webpack-auto-vue-router] ' + x));
+      compilation.warnings = compilation.warnings.concat(this.metaOutput.warnings.map(x => '[webpack-auto-vue-router] ' + x));
+      cb && cb();
+    });
+    this._hook(compiler, 'done', 'done', (compilation, cb) => {
+      if (!this.watcher) {
+        this.watcher = chokidar.watch(this.options.entry, {
+          ignoreInitial: true
+        });
+        let changeCb = this._fileChanged.bind(this);
+        let removeCb = this._fileRmoved.bind(this);
+        this.watcher.on('add', changeCb).on('change', changeCb).on('unlink', removeCb);
+      }
+      cb && cb();
+    })
+    this.start();
+  }
+
+  _isVue(path) {
+    return /\.vue$/g.test(path);
+  }
+
+  _fileChanged(file) {
+    log('fileChanged', file);
+    this._isVue(file) && this._diffConfig(file);
+  }
+
+  _fileRmoved(file) {
+    log('fileRmoved', file);
+    this._isVue(file) && this.start();
+  }
+
+  _hook(compiler, v3Name, v4Name, cb) {
+    if (compiler.hooks && compiler.hooks[v4Name]) {
+      compiler.hooks[v4Name].tap('VueAutoRouteWebapckPlugin', cb);
+    } else {
+      compiler.plugin(v3Name, cb);
+    }
+  }
+
+  start() {
+    let filesPath = this.fetchFiles(this.options.entry);
+    this.routes = [];
+    this.filesPath = filesPath;
+    this.parseFiles();
+    this.buildRoute();
+  };
+
+  _diffConfig(file) {
+    let config = this.parseRouteConfig(file);
+    let _config = this.files[file];
+    if (_config) {
+      let hasDiff = diff(_config, config);
+      if (hasDiff) {
+        this.files[file] = config;
+        this.updateOneRoute(config);
+      };
+    } else {
+      this.files[file] = config;
+      this.pushOneRoute(config);
+      this.output();
+    }
+  }
+
+  updateOneRoute(config) {
+    let {
+      _path,
+      name,
+      meta,
+      lazy
+    } = config;
+    let route = this.routesMap[_path];
+    route.name = name;
+    route.meta = meta;
+    route.components.default = lazy ? `${lazy}|lazy|${_path}` : _path;
+    this.output();
+  }
+
+  parseFiles() {
+    this.filesPath.forEach(path => {
+      this.files[path] = this.parseRouteConfig(path);
+    });
+  }
+
+  checkDuplicate() {
+    let data = {};
+    this.metaOutput.warnings = [];
+    for (let _path in this.routesMap) {
+      let name = this.routesMap[_path].name;
+      if (name) {
+        if (data[name]) {
+          this.metaOutput.warnings.push(`Duplicate named routes definition: [${_path},${data[name]}]`);
+        } else {
+          data[name] = _path;
+        }
+      }
+    }
+    data = null;
+  }
+  pushOneRoute(config) {
+    let pathItems = config._path.replace(this.options.entry, '').split('/');
+    this.touchRoute(this.routes, null, pathItems, config);
+  }
+
+  buildRoute() {
+    this.filesPath.forEach(path => {
+      let routeConfig = this.files[path];
+      this.pushOneRoute(routeConfig);
+    });
+    this.output();
+  }
+
+  output() {
+    let routes = JSON.stringify(this.routes);
+    routes = routes.replace(/"default":(".+?\.vue")/g, (v1, v2) => {
+      let _v2 = v2.slice(1, -1).split('|lazy|');
+      if (_v2.length === 1) {
+        return `default:require('${_v2[0]}').default`;
+      } else if (_v2.length === 2) {
+        let webpackChunkName = _v2[0] === 'true' ? '' : `/*webpackChunkName:"${_v2[0]}"*/`;
+        return `default:()=>import(${webpackChunkName}"${_v2[1]}")`;
+      } else {
+        return `default:require('${v2}').default`;
+      }
+    });
+    fs.writeFileSync(this.options.output, `export default ${routes}`);
+    this.checkDuplicate();
+  }
+
+  touchRoute(route, parentRoute, pathItems, routeConfig) {
+    let {
+      rootComponent,
+      indexComponent,
+      useFileName
+    } = this.options;
+    let {
+      _path,
+      meta,
+      name,
+      lazy
+    } = routeConfig;
+    let _reoutes = parentRoute && parentRoute.children || route;
+    let pathItem = pathItems.shift();
+    let components = {};
+    if (this._isVue(pathItem)) {
+      let _path_ = lazy ? `${lazy}|lazy|${_path}` : _path;
+      pathItem = pathItem.replace('.vue', '');
+      // if (routeConfig.view) {
+      //   if (parentRoute) {
+      //     parentRoute.components[routeConfig.view] = _path_;
+      //   } else {
+      //     components[routeConfig.view] = _path_;
+      //   }
+      // } else {
+      //   components.default = _path_;
+      // }
+      components.default = _path_;
+      if (pathItem === indexComponent) {
+        if (parentRoute) {
+          parentRoute.name = name ? name : (useFileName && `${parentRoute.path}/${pathItem}`);
+          parentRoute.components = Object.assign(parentRoute.components, components);
+          parentRoute.meta = meta;
+          this.routesMap[_path] = parentRoute;
+        }
+      } else {
+        let _route_ = {
+          name: name ? name : (useFileName && pathItem),
+          path: `${parentRoute?pathItem:pathItem === rootComponent ?'/':'/'+pathItem}`,
+          components,
+          meta
+        };
+        this.routesMap[_path] = _route_;
+        _reoutes.push(_route_);
+      }
+    } else {
+      let _route = _reoutes.find(r => r.path === pathItem || r.path === '/' + pathItem);
+      if (!_route) {
+        _route = {
+          path: `${parentRoute?pathItem:'/'+pathItem}`,
+          children: [],
+          components: {}
+        }
+        _reoutes.push(_route);
+      }
+      this.touchRoute(_route.children, _route, pathItems, routeConfig);
+    }
+  }
+
+  fetchFiles(dir) {
+    let files = [];
+    let list = fs.readdirSync(dir);
+    list.forEach(file => {
+      if (file === this.options.ignoreDir) return;
+      let filePath = path.join(dir, file);
+      let stat = fs.statSync(filePath);
+      if (stat && stat.isDirectory()) {
+        files = files.concat(this.fetchFiles(filePath));
+      } else {
+        files.push(filePath);
+      }
+    });
+    return files;
+  }
+
+  parseRouteConfig(filePath) {
+    const descriptor = parse({
+      source: fs.readFileSync(filePath, {
+        encoding: 'utf8'
+      }),
+      compiler: vueCompiler,
+      filename: path.basename(filePath)
+    });
+    let route;
+    let _config = {
+      meta: null,
+      lazy: false,
+      view: null,
+      _path: filePath
+    };
+    let script = descriptor.script ? descriptor.script.content : null;
+    if (script) {
+      script = script.replace(/\/\/[\S\s]+?[\r\n]/g, '').replace(/\/\*[\s\S]+?\*\//g, '');
+      route = this.parseRoute(script);
+      if (route) {
+        try {
+          route = new Function('return ' + route.replace(/[\r\n]+/g, ''))();
+          route = Object.assign(_config, route);
+        } catch (error) {
+          console.error(route);
+          throw new Error(error);
+        }
+      } else {
+        route = _config;
+      }
+    } else {
+      route = _config;
+    }
+    return route;
+  }
+
+  parseRoute(txt) {
+    let data = txt;
+    let mark = this.options.propsKeyName + ':';
+    let chart1 = '{';
+    let chart2 = '}';
+    let startIndex = data.indexOf(mark);
+    let count = 0;
+    let begingIndex;
+    let endingIndex;
+    if (startIndex === -1) {
+      return '';
+    }
+    for (let i = startIndex; i < data.length; i++) {
+      let w = data.charAt(i);
+      if (w === chart1) {
+        if (count === 0) begingIndex = i;
+        ++count;
+      } else if (w === chart2) {
+        --count;
+        if (count === 0) {
+          endingIndex = i + 1;
+          break;
+        }
+      }
+    }
+    return endingIndex ? data.slice(begingIndex, endingIndex) : '';
+  }
+}
+
+module.exports = VueAutoRouteWebapckPlugin;
